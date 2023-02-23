06cfe2a7075eb5930826730fbf1ac174390b5975
beauspot <beauspot@outlook.com>
2023-02-23 15:27:17 +0100

    modified the entery point of this file by setting up a connection to the database

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const exphbs = require("express-handlebars");
 const appRouter = require("./routes/appRoutes");
-const _404ErrorRoute = require("./middleware/_404_Error")
+const _404ErrorRoute = require("./middleware/_404_Error");
+const { sequelize } = require("./models");
 
 const app = express();
 
@@ -14,9 +15,19 @@ app.use("/gigs", appRouter);
 
 app.get("/", (req, res) => res.status(304).redirect("/gigs"));
 
-app.use("*", _404ErrorRoute.get404Response )
+app.use("*", _404ErrorRoute.get404Response);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.info(`Express server listening on port ${PORT}`);
-});
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    app.listen(PORT, () => {
+      console.info(`Express server listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error({ msg: error.message });
+  }
+};
+
+startServer();
